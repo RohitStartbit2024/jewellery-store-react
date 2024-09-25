265afbfe1efb90562e9c6038e5200ce1092a8136
Rohit Agarwal <rohitagarwal@startbitsolutions.com>
2024-09-25 10:47:40 +0530

    review card 2

--- src/modules/about/Clients/Clients.jsx
+++ src/modules/about/Clients/Clients.jsx
@@ -1,7 +1,7 @@
 export default function(props){
     return(
-        <div className="py-2 px-2 md:pt-10 w-[30%] bg-teal-50 text-center">
-            <div className="flex px-4 md:px-20 md:h-[40%] justify-center">
+        <div className="py-2 px-2 md:pt-10 w-full md:w-[30%] aspect-video md:aspect-auto bg-teal-50 text-center">
+            <div className="flex px-4 md:px-20 aspect-square md:h-[40%] justify-center">
                 <img src={props.image} alt="client" className="rounded-full"/>
             </div>
             <p className="mt-2 md:mt-5 text-[10px] md:text-base">{props.review}</p>
