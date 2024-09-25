450e69a7a5c3138041234d9d9634248c5d4516ac
Rohit Agarwal <rohitagarwal@startbitsolutions.com>
2024-09-25 10:23:07 +0530

    card image size

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function(props){
     return(
         <div className='text-center hover:shadow-gray-500 hover:shadow-md w-[45%] ml-[2.5%] md:ml-0 group md:w-[31%] mb-3 aspect-square md:aspect-auto md:h-96'>
-            <img src={props.image} alt="ProductImage"className="min-w-full h-[48%]"/>
+            <img src={props.image} alt="ProductImage"className="min-w-full h-[50%]"/>
             <h1 className='text-xs md:text-lg md:px-5'>{props.name}</h1>
             <p className='text-xs md:text-sm text-gray-500 uppercase'>{props.category}</p>
             <p className='md:mt-3 text-sm mt-1 text-teal-400 font-semibold md:text-lg'>${props.price}</p>
