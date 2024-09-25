1e2ac7f766cee2c37ba3ea0c88c8574125029c0a
Rohit Agarwal <rohitagarwal@startbitsolutions.com>
2024-09-25 10:58:40 +0530

    review card full

--- src/modules/about/Clients/Clients.jsx
+++ src/modules/about/Clients/Clients.jsx
@@ -1,8 +1,8 @@
 export default function(props){
     return(
         <div className="py-2 px-2 md:pt-10 flex mb-2 md:mb-0 md:block md:w-[30%] bg-teal-50 text-center">
-            <div className="flex px-4 md:px-20 w-[30%] aspect-square md:aspect-auto my-auto md:my-0 md:w-auto md:h-[40%] justify-center">
-                <img src={props.image} alt="client" className="rounded-full"/>
+            <div className="flex px-4 md:px-20 w-[30%] aspect-square md:aspect-auto my-auto md:my-0 md:w-auto md:h-[40%] justify-center rounded-full">
+                <img src={props.image} alt="client" className="w-full h-full"/>
             </div>
             <div>
             <p className="mt-2 md:mt-5 text-[10px] md:text-base">{props.review}</p>
