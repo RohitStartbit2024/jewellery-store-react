fc13d61fd6e050ad00d43d07ca4679f5cdd403d1
Rohit Agarwal <rohitagarwal@startbitsolutions.com>
2024-09-25 11:05:12 +0530

    review max-auto

--- src/modules/about/Clients/Clients.jsx
+++ src/modules/about/Clients/Clients.jsx
@@ -1,7 +1,7 @@
 export default function(props){
     return(
         <div className="py-2 px-2 md:pt-10 flex mb-2 md:mb-0 md:block md:w-[30%] bg-teal-50 text-center">
-            <div className="flex w-[30%] aspect-square my-auto md:my-0 md:w-auto md:h-[40%] justify-center">
+            <div className="flex w-[30%] md:mx-auto aspect-square my-auto md:my-0 md:w-auto md:h-[40%] justify-center">
                 <img src={props.image} alt="client" className="w-full h-full rounded-full"/>
             </div>
             <div>
